b1ba8d4c6dec7949260511b78b2fe3ab26322022
Pedro Roig <pedro.roig.gimenez@gmail.com>
2015-07-30 05:43:10 +0200

    CheckPoint del proyecto: Lesson 1 / Adapters

--- app/src/main/java/com/example/android/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/MainActivityFragment.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -17,6 +20,15 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        List<String> forecastList = new ArrayList<String>();
+        forecastList.add("Today - Sunny - 21/33");
+        forecastList.add("Tomorrow - Cloudy - 20/29");
+        forecastList.add("Saturday - Sunny - 21/34");
+        forecastList.add("Sunday - Sunny - 20/30");
+
         return inflater.inflate(R.layout.fragment_main, container, false);
+
+
+
     }
 }
