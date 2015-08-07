4d51e21b5eda533b2f8473096dc1c45aa66dc5a0
unknown <T09312@ES-D-204925H.iberia.grupotecnocom>
2015-08-07 17:21:48 +0200

    lesson3, chapter 12

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -1,11 +1,16 @@
 package com.example.android.sunshine.app;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ShareActionProvider;
 
 public class SettingsActivity extends ActionBarActivity {
+
+    ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +19,22 @@ public class SettingsActivity extends ActionBarActivity {
     }
 
 
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_settings, menu);
+
+        // Get the menu item.
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        // Get the provider and hold onto it to set/change the share intent.
+        mShareActionProvider = (ShareActionProvider) menuItem.getActionProvider();
+        // Set history different from the default before getting the action
+        // view since a call to MenuItem.getActionView() calls
+        // onCreateActionView() which uses the backing file name. Omit this
+        // line if using the default share history file is desired.
+        mShareActionProvider.setShareHistoryFileName("custom_share_history.xml");
+
         return true;
     }
 
